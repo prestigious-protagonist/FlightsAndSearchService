685cbd3308f2ad4c6720768d24005b764a6ec08a
prestigious-protagonist <jaskaranyt123@gmail.com>
2024-11-04 16:49:10 +0530

    Setup Basic body-Parser

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const setupAndStartServer = async () => {
 
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({extended : true}))
+    
     app.listen( PORT,()=> {
         console.log(`On port ${PORT}`)
     })
