7960922305d4174ac3e1898c02427d05d7e4d2c6
prestigious-protagonist <jaskaranyt123@gmail.com>
2024-11-04 22:10:27 +0530

    adding up the city repo

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,1 +1,28 @@
+
 const {City} = require('../models/index')
+
+const {City} = require('../models/index')
+
+class CityRepository{
+    async createCity({name}){
+        try {
+            const city = await City.createCity({name});
+            return city;
+        } catch (error) {
+            throw {error};
+        }
+    }
+    async deleteCity(cityId){
+        try {
+            await City.destroy({
+                where:{
+                    id : cityId
+                }
+            })
+        } catch (error) {
+            throw {error};
+        }
+    }
+}
+
+module.exports = CityRepository;
